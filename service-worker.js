2e8d6c58f70eb89b094254419304c432445c36e6
weblogger117-art <weblogger117@gmail.com>
2026-09-25 15:38:11 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "xbox-game-archive-v2";
+const CACHE_NAME = "xbox-game-archive-v3";
 
 const APP_FILES = [
     "./",
@@ -8,79 +8,46 @@ const APP_FILES = [
     "./manifest.json"
 ];
 
-
-/* =========================================================
-   INSTALLATION
-   ========================================================= */
-
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
         caches.open(CACHE_NAME)
-            .then(cache => {
-
-                return cache.addAll(APP_FILES);
-
-            })
-
+            .then(cache => cache.addAll(APP_FILES))
     );
 
     self.skipWaiting();
-
 });
 
-
-/* =========================================================
-   AKTIVIERUNG
-   ========================================================= */
-
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
-        caches.keys()
-            .then(cacheNames => {
-
-                return Promise.all(
-
-                    cacheNames
-                        .filter(
-                            cacheName =>
-                                cacheName !== CACHE_NAME
-                        )
-                        .map(
-                            cacheName =>
-                                caches.delete(cacheName)
-                        )
-
-                );
-
-            })
-
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
     );
 
     self.clients.claim();
-
 });
 
-
-/* =========================================================
-   ANFRAGEN
-   ========================================================= */
-
 self.addEventListener("fetch", event => {
-
     event.respondWith(
+        fetch(event.request)
+            .then(response => {
 
-        caches.match(event.request)
-            .then(cachedResponse => {
+                if (response && response.status === 200) {
+                    const responseClone = response.clone();
 
-                return cachedResponse ||
-                    fetch(event.request);
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
+                }
 
+                return response;
             })
-
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
-
 });
